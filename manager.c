f8aab301038b90560925ce879b4921f20a0950ca
2100673-YS-Jeong <21800673@handong.edu>
2022-04-25 22:47:45 +0900

    file

--- manager.c
+++ manager.c
@@ -41,6 +41,11 @@ void saveData(Product p[], int count){
 	//중량 가격 제품명
 	fp= fopen("product.txt","wt");
 	
+	for(int i = 0; i < count; i++) {
+		if(p[i].weight == -1)
+			continue;
+		fprintf(fp, "%d %d %s\n", p[i].weight, p[i].price, p[i].name);
+	}
 	
 	fclose(fp);
 	printf("저장됨!\n");
@@ -52,10 +57,21 @@ int loadData(Product *p){
 	int count=0;
 	FILE*fp;
 
-	//파일 내용을 읽어와서 배열에 값 추가하기
+	//파일 내용을 읽어와서 배열에 값 추가하기	
+	fp= fopen("product.txt","rt");
+	
+	if(fp == NULL)
+		return 0;
 
-
-
+	for(int i = 0; i < 100; i++) {
+		if(feof(fp))
+			break;
+		fscanf(fp, "%d", &p[i].weight);
+		fscanf(fp, "%d", &p[i].price);
+		fscanf(fp, "%[^\n]s", p[i].name);
+		count++;
+	}
+	count--;
 
 	printf("=> 로딩 성공!\n");
 	return count;
